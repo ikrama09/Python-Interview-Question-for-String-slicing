2eaefbdffb938e28bd2ce3ca1b4f83013315d824
ikrama09 <144513088+ikrama09@users.noreply.github.com>
2026-06-22 18:20:38 +0530

    Add files via upload

---
+++ string_slicing.py
@@ -0,0 +1,44 @@
+'''Q=1 Print "Python"
+Print "Programming"'''
+
+txt = "PythonProgramming"
+print(txt[:6])
+print(txt[6:])
+
+'''Q=2 Print the first 10 characters.
+Print the last 12 characters.
+Print every second character.'''
+
+txt1 = "Artificial Intelligence"
+print(txt1[:11])
+print(txt1[-12:])
+print(txt1[::2])
+
+'''Q=3 Reverse the string.
+Print every alternate character.
+Print "AI".'''
+
+txt2 = "OpenAI"
+print(txt2[::-1])
+print(txt2[::2])
+print(txt2[-2:])
+
+'''Q=4 Print "Python"
+Print "fun"
+Reverse the entire sentence.'''
+
+sentence = "Learning Python is fun"
+print(sentence[9:15])
+print(sentence[-3:])
+print(sentence[::-1])
+
+'''Q=5 Print every 3rd character.
+Print the alphabet in reverse.
+Print characters from 'f' to 'p'.
+Print every second character from 'z' to 'a'.'''
+
+data = "abcdefghijklmnopqrstuvwxyz"
+print(len(sentence))
+print(data[::-1])
+print(data[5:16])
+print(data[::-2])
